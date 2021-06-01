6dc15a640e654ece719b66b1c10a38c885066072
MDolphin259 <77594072+MDolphin259@users.noreply.github.com>
2021-05-31 22:49:23 -0500

    Add files via upload

---
+++ DisorderData.csv.js
@@ -0,0 +1,9 @@
+﻿var sortBarData = `Diagnosis,Total
+Anxiety Disorder,373
+Mood Disorder,526
+ADHD,134
+PTSD,70
+Personality Disorder,28
+Substance Use Disorder,40
+Addictive Disorder,15
+Eating Disorder,18`
